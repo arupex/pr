9bd65cc8a7f8d928b7d3f022564da4cdc14f86cb
Daniel Irwin <daniel.irwin@enernoc.com>
2016-06-28 10:42:25 -0400

    screwed up namespacing...

--- core-plugins/github.js
+++ core-plugins/github.js
@@ -5,15 +5,17 @@ module.exports = function(opts, state, cb){
 
     var GithubApi = require('github');
 
+    var githubOpts = opts.plugins.github;
+
     var github = new GithubApi({
-        protocol : opts.plugin.github.protocol || 'https',
-        host : opts.plugin.github.host || 'api.github.com',
-        timeout : opts.plugin.github.timeout || 5000,
-        pathPrefix: opts.plugin.github.pathPrefix
+        protocol : githubOpts.protocol || 'https',
+        host : githubOpts.host || 'api.github.com',
+        timeout : githubOpts.timeout || 5000,
+        pathPrefix: githubOpts.pathPrefix
     });
 
     github.repos.getCommits({
-        per_page : opts.plugin.github.pageSize || 200,
+        per_page : githubOpts.pageSize || 200,
         user : state.user,
         repo : state.repo
     }, function(err, data){
